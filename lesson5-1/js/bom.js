7ef27661fa1d02429e339df2f14fc75a13708379
Angel-L-T <lug21003@byui.edu>
2021-10-12 22:40:21 -0500

    DOM BoM Favorite List

---
+++ lesson5-1/js/bom.js
@@ -0,0 +1,24 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
+
+button.addEventListener("click", 
+    function() {
+        if (input.value != "") {
+            const listItem = document.createElement("li");
+            const buttonItem = document.createElement("button");
+
+            listItem.textContent = input.value;
+            buttonItem.textContent = "❌";
+
+            listItem.appendChild(buttonItem);
+
+            list.appendChild(listItem);
+
+            buttonItem.addEventListener("click", function() {list.removeChild(listItem);})
+        }
+
+        input.value = '';
+        input.focus();
+    }
+);
