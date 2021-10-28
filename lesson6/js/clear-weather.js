fdd14a7187d3e2bb30f61395033ca4de17d55121
Angel-L-T <lug21003@byui.edu>
2021-10-27 22:26:44 -0500

    changed js code order

--- lesson6/js/clear-weather.js
+++ lesson6/js/clear-weather.js
@@ -1,16 +1,17 @@
 // Code to display date on the bottom.
 const currentdate = new Date();
 const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-const formatDate = currentdate.toLocaleDateString('en-UK', options);
 
-document.querySelector("footer span").textContent = formatDate;
+function footDate(currentdate, options) {
+    document.querySelector("footer span").textContent = currentdate.toLocaleDateString('en-UK', options);
+}
 
+footDate(currentdate, options);
 
 // Code for the responsive navigation menu.
 function toggleMenu() {
     document.getElementsByClassName("responsive-nav")[0].classList.toggle("responsive");
 }
-
 
 // Down here is the code to show that on Saturday there will be pancakes.
 const dayOption = {weekday: 'long'}
--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -3,11 +3,14 @@ const temp = document.getElementById("temp").innerHTML;
 const mph = document.getElementById("mph").innerHTML;
 const windChillSpace = document.getElementById("wind-chill");
 
-if (temp <= 50 && mph > 3) {
+function calcWindchill(temp, mph) {
     const windFactor = 35.74 + 0.6215*(temp) - 35.75*(mph)**0.16 + 0.4275*(temp)*(mph)**0.16
     const windChill = Math.round(windFactor);
-    windChillSpace.innerHTML = windChill + " °F";
-    // windChillSpace.innerHTML = `${windChill} °F`; <-- How to do it with a Template Literal.
+    windChillSpace.innerHTML = windChill + " °F"; // windChillSpace.innerHTML = `${windChill} °F`; <-- How to do it with a Template Literal.
+}
+
+if (temp <= 50 && mph > 3) {
+    calcWindchill(temp, mph);
 }
 
 else {
