2eb93ff4552fd84514625b788616707f101aec66
Angel-L-T <lug21003@byui.edu>
2021-11-22 23:07:14 -0600

    Weather Site, pre-final version

---
+++ lesson11/js/windchill.js
@@ -0,0 +1,18 @@
+    // Code for the wind chill calculator.
+const temp = document.getElementById("temp").textContent;
+const mph = document.getElementById("mph").textContent;
+const windChillSpace = document.getElementById("wind-chill");
+
+function calcWindchill(temp, mph) {
+    const windFactor = 35.74 + 0.6215*(temp) - 35.75*(mph)**0.16 + 0.4275*(temp)*(mph)**0.16;
+    const windChill = Math.round(windFactor);
+    windChillSpace.innerHTML = windChill + " °F"; // windChillSpace.innerHTML = `${windChill} °F`; <-- How to do it with a Template Literal.
+}
+
+if (temp <= 50 && mph > 3) {
+    calcWindchill(temp, mph);
+}
+
+else {
+    windChillSpace.innerHTML = "N/A"
+}
