89e9644c64649818972af9a4adaed6b066b0cb03
Angel-L-T <lug21003@byui.edu>
2021-12-12 22:26:59 -0600

    Home Page Almost Complete

---
+++ neza-chamber-of-commerce/js/events.js
@@ -0,0 +1,32 @@
+//#region -----------DECLARING FUNCTIONS-----------
+
+function upcomingEvents () {
+    const eventsUrl = "https://angel-l-t.github.io/wdd230/neza-chamber-of-commerce/json/neza-events.json";
+
+    fetch(eventsUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        // console.table(jsonObject);  // temporary checking for valid response and data parsing
+        
+        const nezaEvents = jsonObject['events'];
+            
+        nezaEvents.forEach(event => {
+            let eventsContent = document.createElement("p");
+
+            eventsContent.innerHTML = event;
+
+            document.getElementById("events").appendChild(eventsContent);
+        });
+    }
+  );
+}
+
+//#endregion -----------DECLARING FUNCTIONS-----------
+
+//#region -----------CALLING FUNCTIONS-----------
+
+upcomingEvents();
+
+//#endregion -----------CALLING FUNCTIONS-----------
---
+++ neza-chamber-of-commerce/js/weather.js
@@ -0,0 +1,45 @@
+
+function weatherInfo () {
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=19.4116&lon=-99.0212&units=imperial&exclude=minutely,hourly&appid=8647c7488ee65eeef25bce2fbb5f6304"
+
+    fetch(weatherUrl)
+    .then(
+        function (response) {
+            return response.json();
+        }
+    )
+    .then(
+        function (weatherObject) {
+            console.table(weatherObject);  // temporary checking for valid response and data parsing
+            let currentTemp = weatherObject["current"];
+            let dailyTemp = weatherObject["daily"];
+
+            let temp = document.getElementById("temperature");
+            let condition = document.getElementById("condition");
+            let humidity = document.getElementById("humidity");
+
+            temp.innerHTML = `${currentTemp.temp} °F`;
+            condition.innerHTML = currentTemp.weather[0].main;
+            humidity.innerHTML = `${currentTemp.humidity}`
+
+            dailyTemp.forEach((element, i) => {
+                let forecastDay = new Date(element.dt * 1000);
+                const shortDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+                
+                if (i == 1 || i == 2 || i == 3) {
+                    let forDay = document.getElementById(`day${i}`);
+                    let forImg = document.getElementById(`img${i}`);
+                    let forTemp = document.getElementById(`forTemp${i}`);
+
+                    forDay.innerHTML = shortDays[forecastDay.getDay()];
+                    forImg.setAttribute("src", `https://openweathermap.org/img/w/${element.weather[0].icon}.png`);
+                    forImg.setAttribute("alt", element.weather[0].description);
+                    forTemp.innerHTML = `${element.temp.day} °F`;
+                }
+            });
+            
+        }
+    );
+}
+
+weatherInfo();
